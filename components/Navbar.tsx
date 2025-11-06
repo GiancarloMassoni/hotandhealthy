0fc7a3f9a0d9b6da2da92fe5461d96d096a0179c
Giancarlo Massoni <massonicarlo55@gmail.com>
2025-11-06 11:04:56 -0800

    Changed img

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -55,7 +55,7 @@ export default function Navbar() {
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
-                <img
+                <Image
                   alt=""
                   src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
                   className="h-8 w-auto"
