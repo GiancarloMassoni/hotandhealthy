7a573bf84d69555d17e1030f5094122e8e81cf60
Giancarlo Massoni <massonicarlo55@gmail.com>
2025-11-14 15:51:24 -0800

    Changed anchor to Link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,14 +19,14 @@ export default function Navbar() {
     <header className="absolute inset-x-0 top-0 z-50 bg-secondary">
         <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
           <div className="flex lg:flex-1">
-            <a href="/" className="-m-1.5 p-1.5">
+            <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <Image
                 alt=""
                 src={kbLogo}
                 className="h-8 w-auto"
               />
-            </a>
+            </Link>
           </div>
           <div className="flex lg:hidden">
             <button
